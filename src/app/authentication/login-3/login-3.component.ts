3505ff41825fd6e1744a710b9f48739f0febe213
Elias TG <tg.elias.s@gmail.com>
2022-10-25 20:28:11 -0500

    Comunicación con Back en login

---
+++ src/app/authentication/login-3/login-3.component.ts
@@ -0,0 +1,88 @@
+import { Component} from '@angular/core';
+import { FormBuilder, FormGroup,  Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../shared/services/authentication.service';
+
+
+@Component({
+    templateUrl: './login-3.component.html'
+})
+
+export class Login3Component{
+
+    loginForm: FormGroup;
+
+    usuario: string;
+    contra: string;
+    
+
+    submitForm(): void {
+        for (const i in this.loginForm.controls) {
+            this.loginForm.controls[ i ].markAsDirty();
+            this.loginForm.controls[ i ].updateValueAndValidity();
+        }
+        const usuario = this.loginForm.controls.userName.value;
+        const contra = this.loginForm.controls.password.value;
+
+        this.authService.login(usuario, contra).subscribe(
+            data => {
+                console.log(data);
+                this.router.navigate(['/dashboard']);
+
+                localStorage.setItem('username', data.username);
+                localStorage.setItem('role', data.role);
+                
+            },
+            error => {
+                // console.log(error);
+                this.usuario = '';
+                this.contra = '';
+                this.router.navigate(['/authentication/login-3']);
+                alert('usuario o contraseña incorrectos');
+            }
+        );
+
+       
+        // use authService subscribe to login
+        // this.authService.login(this.usuario, this.contra).subscribe(
+        //     (data) => {
+        //         console.log(data);
+        //         // this.router.navigate(['/dashboard']);
+        //     },
+        //     (error) => {
+        //         console.log(error);
+        //     }
+        // );
+
+        // console.log(this.loginForm.controls.userName.value);
+        // console.log(this.loginForm.controls.password.value);
+
+
+
+        // console.log(this.loginForm.controls.userName.value);
+        // console.log(this.loginForm.controls.password.value);
+        // let user = this.loginForm.controls.userName.value;
+        // let password = this.loginForm.controls.password.value;
+        // if (this.usuario === "elias" && this.contra === "elias") {
+        //     this.login = true;
+        //     this.router.navigate(['/dashboard']);
+        // }
+        // else if (this.usuario.length <= 0 || this.contra.length<=0) {
+        //     // alert("Todos los campos son obligatorios");
+        // }
+        // else{
+        //     this.login = false;
+        //     alert("Usuario o contraseña incorrectos");
+        // }
+    }
+
+    constructor(private fb: FormBuilder, public router: Router, private authService: AuthenticationService) {
+    }
+
+    ngOnInit(): void {
+        this.loginForm = this.fb.group({
+            userName: [ null, [ Validators.required ] ],
+            password: [ null, [ Validators.required ] ]
+        });
+    }
+}    
---
+++ src/app/shared/template/header/header.component.ts
@@ -0,0 +1,80 @@
+import { Component, Input } from '@angular/core';
+import { ThemeConstantService } from '../../services/theme-constant.service';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+
+export class HeaderComponent{
+
+    searchVisible : boolean = false;
+    quickViewVisible : boolean = false;
+    isFolded : boolean;
+    isExpand : boolean;
+
+    // asignar valor email del local storage a la variable email
+    username = localStorage.getItem('username');
+    role = localStorage.getItem('role');
+
+    logout() {
+        localStorage.removeItem('username');
+        localStorage.removeItem('role');
+        this.router.navigate(['/authentication/login-3']);
+    }
+
+    constructor( private themeService: ThemeConstantService, private router: Router) {}
+
+    ngOnInit(): void {
+        this.themeService.isMenuFoldedChanges.subscribe(isFolded => this.isFolded = isFolded);
+        this.themeService.isExpandChanges.subscribe(isExpand => this.isExpand = isExpand);
+    }
+
+    toggleFold() {
+        this.isFolded = !this.isFolded;
+        this.themeService.toggleFold(this.isFolded);
+    }
+
+    toggleExpand() {
+        this.isFolded = false;
+        this.isExpand = !this.isExpand;
+        this.themeService.toggleExpand(this.isExpand);
+        this.themeService.toggleFold(this.isFolded);
+    }
+
+    searchToggle(): void {
+        this.searchVisible = !this.searchVisible;
+    }
+
+    quickViewToggle(): void {
+        this.quickViewVisible = !this.quickViewVisible;
+    }
+
+    notificationList = [
+        {
+            title: 'You received a new message',
+            time: '8 min',
+            icon: 'mail',
+            color: 'ant-avatar-' + 'blue'
+        },
+        {
+            title: 'New user registered',
+            time: '7 hours',
+            icon: 'user-add',
+            color: 'ant-avatar-' + 'cyan'
+        },
+        {
+            title: 'System Alert',
+            time: '8 hours',
+            icon: 'warning',
+            color: 'ant-avatar-' + 'red'
+        },
+        {
+            title: 'You have a new update',
+            time: '2 days',
+            icon: 'sync',
+            color: 'ant-avatar-' + 'gold'
+        }
+    ];
+}
